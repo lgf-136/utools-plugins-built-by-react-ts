e85055e9b44d2e31ea6913a585d473c6438ce72e
lgf <lgf_136@163.com>
2022-09-25 17:22:32 +0800

    fix: 🐛 generate whole framework
    generate whole framework

---
+++ src/pages/Todos/Main/TodoList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+export default function TodoList() {
+  return (
+    <div>
+      <h2>TodoList</h2>
+      <TodoItem/>
+    </div>
+  );
+}
