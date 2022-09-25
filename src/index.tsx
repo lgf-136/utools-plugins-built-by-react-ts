3c80a80dee34688b690b4f35351aa067752ab7e1
lgf <lgf_136@163.com>
2022-09-25 17:22:32 +0800

    fix: 🐛 generate whole framework
    generate whole framework

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './styles/reference.css';
+
+/*
+ * yarn add todomvc-app-css
+ * import 'todomvc-app-css/index.css'
+ */
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode>
   <App />
